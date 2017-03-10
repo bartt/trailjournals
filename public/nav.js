ceea3bd96ac8be9e97b942693d546edb7060fcfa
Bart Teeuwisse <bart.teeuwisse@thecodemill.biz>
2017-03-10 06:48:18 -0800

    - Add keyboard navigation

--- public/nav.js
+++ public/nav.js
@@ -1,16 +1,18 @@
 function addNavLinks() {
   var id = window.location.search.split('=').pop();
   if (id) {
-    getNavIds(id).then(function(links) {
-      var nav = document.querySelector('.nav');
-      var html = '';
-      links.forEach(function(link){
-        html += '<a href="/entry?id=' + link.id + '">' + link.text + '</a> '
+    getNavIds(id)
+      .then(function(links) {
+        var nav = document.querySelector('.nav');
+        var html = '';
+        links.forEach(function(link){
+          html += '<a href="/entry?id=' + link.id + '" id="' + link.text + '">' + link.text + '</a> '
+        })
+        nav.innerHTML = html
       })
-      nav.innerHTML = html
-    })
+      .then(addNavKeyListener);
   }
-}
+};
 
 function getNavIds(id) {
   return fetch(new Request('/proxy?id=' + id))
@@ -29,6 +31,33 @@ function getNavIds(id) {
           id: l.getAttribute('href').split('=').pop()}
         });
     });
-}
+};
+
+function addNavKeyListener() {
+  document.addEventListener('keydown', function(event) {
+    var navId;
+    switch (event.keyCode) {
+      case 70 /* f */:
+        navId = 'first';
+        break;
+      case 74 /* j */:
+        navId = 'previous';
+        break;
+      case 75 /* k */:
+        navId = 'next';
+        break;
+      case 76 /* l */:
+        navId = 'last';
+        break;
+      default:
+    }
+    if (navId) {
+      var navLink = document.querySelector('a#' + navId);
+      if (navLink) {
+        navLink.click();
+      }
+    }
+  })
+};
 
 addNavLinks();
