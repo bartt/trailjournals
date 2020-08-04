6e540b8168a92f397294f02a4d2f16e68be98b2e
Bart Teeuwisse <bart@thecodemill.biz>
2020-08-04 13:44:40 -0700

    Don't use protocols in views

--- app.rb
+++ app.rb
@@ -132,9 +132,9 @@ class TrailJournals < Sinatra::Base
       :entry_id => entry_id,
       :date => date,
       :hiker_id => hiker_id,
-      :avatar_href => avatar_href.gsub(/^http:/, ''),
+      :avatar_href => avatar_href,
       :stats => stats,
-      :img_href => img_href.gsub(/^http:/, ''),
+      :img_href => img_href,
       :body => body,
       :signature => signature,
       :request => request
